dfaee8b62918546db3751cfa76be75fc1aa19e9b
Manu Fuentes <manufuentes28@gmail.com>
2022-01-17 15:59:58 -0300

    add changes

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,69 @@
+from typing import Dict
 import pandas as pd
+import json
+from pandas.core.frame import DataFrame
 
-frame_data = {'name': ['James', 'Jason', 'Rogers'], 'age': [18, 20, 22], 'job': ['Assistant', 'Manager', 'Clerk']}
+FILE = './datos_data_engineer.tsv'
+SETTINGS = './settings.json'
+SEP = '\t'
+ENCODING = 'utf-16le'
 
-df = pd.DataFrame(frame_data)
-print(df)
+def json_settings(json_name: str) -> Dict:
+    """[summary]
+
+    Args:
+        json_name (str): [description]
+
+    Returns:
+        Dict: [description]
+    """
+    with open(json_name) as settings:
+        json_content = json.load(settings)
+    return json_content
+
+def read_csv_pandas(filename: str, sep: str=',', encoding: str='utf-8') -> DataFrame:
+    """[summary]
+
+    Args:
+        filename (str): [description]
+        sep (str, optional): [description]. Defaults to ','.
+        encoding (str, optional): [description]. Defaults to 'utf-8'.
+
+    Returns:
+        DataFrame: [description]
+    """
+    return pd.read_csv(filename, sep = sep, encoding=encoding)
+
+if __name__ == "__main__":
+    df = read_csv_pandas(FILE, SEP, ENCODING)
+    count_row = df.shape[0]
+    count_columns = df_shape[1]
+    data = json_settings(SETTINGS)
+
+    #print(df)
+
+    #print(data)
+
+    # Primer validacion
+    if count_columns != len(columns):
+        
+        raise Exception("El numero de columnas es diferente")
+
+    # Validacion de duplicados
+    df.drop_duplicates(inplace=True)
+    print("Elimincacion de duplicados")
+    print(f"La cantidad de filas del dataframe es: {df.shape[0]}")
+
+    # Validacion de enteros
+
+    # Validacion de nulos
+
+    # Validacion de string
+
+    # Validacion de regex
+
+    # Porcentaje de null por fila
+
+    # Validar porcentaje de dataframe valido
+
+    # Devolver los datos con errores
