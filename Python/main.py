20693c9814c904ef3cc49ae5b1557d0634b07943
AndresSalvatierra <andressalvatierra11@gmail.com>
2022-01-17 16:29:12 -0300

    Falta dockerizar

--- Python/main.py
+++ Python/main.py
@@ -1,3 +1,4 @@
+from asyncore import write
 from typing import Dict
 import pandas as pd
 import json
@@ -8,8 +9,10 @@ from column import Columna
 
 FILE = './datos_data_engineer.tsv'
 SETTINGS = './settings.json'
-SEP = '\t'
-ENCODING = 'utf-16le'
+SEP_IN = '\t'
+SEP_OUT = '\t'
+ENCODING_IN = 'utf-16le'
+ENCODING_OUT = 'utf-8'
 
 
 logging.basicConfig(filename='./loggers.log', format='%(levelname)s - %(asctime)s - %(message)s', level=logging.INFO)
@@ -42,8 +45,18 @@ def read_csv_pandas(filename: str, sep: str =',', encoding: str='utf-8') -> Data
     """
     return pd.read_csv(filename, sep = sep, encoding=encoding)
 
+def write_csv_pandas(dataframe: DataFrame, sep: str=',', encoding: str='utf-8') -> None:
+    """[summary]
+
+    Args:
+        dataframe (DataFrame): [description]
+        sep (str, optional): [description]. Defaults to ','.
+        encoding (str, optional): [description]. Defaults to 'utf-8'.
+    """
+    dataframe.to_csv('final.csv', sep = sep, encoding = encoding, index=False)
+
 if __name__ == "__main__":
-    df = read_csv_pandas(FILE,SEP,ENCODING)
+    df = read_csv_pandas(FILE,SEP_IN,ENCODING_IN)
     count_row = df.shape[0]
     count_columns = df.shape[1]
     count_nan = int((40*count_columns)/100)
@@ -70,21 +83,30 @@ if __name__ == "__main__":
         object_column = Columna(settings[columna])
         # Validacion de unicos
         if object_column.unique:
+            logging.info(columna)
             df.drop_duplicates(subset=[columna], inplace=True)
+            logging.info(f"La cantidad de filas del dataframe despues de las validaciones: {df.shape[0]}")
 
         # Validacion de tipo
         if object_column.type == 'int':
             # Valida enteros, vacios y nulos
             df = df[~pd.to_numeric(df[columna], errors='coerce').isnull()]
-        else:
+        else: # String
             # Validacion de null
             if object_column.not_null:
+                logging.info(columna)
                 df.dropna(subset=[columna], inplace=True)
+                logging.info(f"La cantidad de filas del dataframe despues de las validaciones: {df.shape[0]}")
 
-        # Validacion de regex
+            # Validacion de regex
+            if object_column.regex:
+                df[columna] = df[columna].str.strip()
+                df = df[df[columna].str.match(object_column.regex)==True]
 
     # Validar porcentaje de dataframe valido
     logging.info(f"La cantidad de filas del dataframe original es: {count_row}")
     logging.info(f"La cantidad de filas del dataframe despues de las validaciones: {df.shape[0]}")
     logging.info(f"La cantidad de filas eliminadas: {count_row - df.shape[0]}")
     logging.info(f"La calidad del dataframe es del: {(df.shape[0]/count_row)*100}%")
+    
+    write_csv_pandas(df, SEP_OUT, ENCODING_OUT)
