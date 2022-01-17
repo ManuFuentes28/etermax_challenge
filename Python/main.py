b26ea25ccd361825e25f02eb977ab442e90eed51
Manu Fuentes <manufuentes28@gmail.com>
2022-01-17 17:28:28 -0300

    add code, gitignore

--- Python/main.py
+++ Python/main.py
@@ -1,12 +1,20 @@
+from asyncore import write
 from typing import Dict
 import pandas as pd
 import json
+import logging
+import re
 from pandas.core.frame import DataFrame
+from column import Columna
 
 FILE = './datos_data_engineer.tsv'
 SETTINGS = './settings.json'
-SEP = '\t'
-ENCODING = 'utf-16le'
+SEP_IN = '\t'
+SEP_OUT = '|'
+ENCODING_IN = 'utf-16le'
+ENCODING_OUT = 'uft-8'
+
+logging.basicConfig(filename='./loggers.log', format='%(levelname)s - %(asctime)s - %(message)s', level=logging.INFO)
 
 def json_settings(json_name: str) -> Dict:
     """[summary]
@@ -32,38 +40,66 @@ def read_csv_pandas(filename: str, sep: str=',', encoding: str='utf-8') -> DataF
     Returns:
         DataFrame: [description]
     """
-    return pd.read_csv(filename, sep = sep, encoding=encoding)
+    return pd.read_csv(filename, sep=sep, encoding=encoding)
+
+def write_csv_pandas(dataframe: DataFrame, sep: str=',', encoding: str='uft-8') -> None:
+    """[summary]
+
+    Args:
+        dataframe (DataFrame): [description]
+        sep (str, optional): [description]. Defaults to ','.
+        encoding (str, optional): [description]. Defaults to 'uft-8'.
+
+    Raises:
+        Exception: [description]
+    """
+    dataframe.to_csv('final.csv', sep=SEP_OUT, encoding=ENCODING_OUT)
 
 if __name__ == "__main__":
-    df = read_csv_pandas(FILE, SEP, ENCODING)
+    df = read_csv_pandas(FILE, SEP_IN, ENCODING_IN)
     count_row = df.shape[0]
     count_columns = df_shape[1]
-    data = json_settings(SETTINGS)
-
-    #print(df)
-
-    #print(data)
-
+    count_nan = int((40*count_columns)/100) # porcentaje de para nulos 
+    logging.info(f"La cantidad de filas del dataframe es: {count_row}")
+    settings = json_settings(SETTINGS)
+    
+    columns = settings['column_list']
+    
     # Primer validacion
-    if count_columns != len(columns):
-        
-        raise Exception("El numero de columnas es diferente")
-
-    # Validacion de duplicados
     df.drop_duplicates(inplace=True)
-    print("Elimincacion de duplicados")
-    print(f"La cantidad de filas del dataframe es: {df.shape[0]}")
-
-    # Validacion de enteros
-
-    # Validacion de nulos
-
-    # Validacion de string
-
-    # Validacion de regex
-
+    logging.info("------- Eliminacion de duplicados -------")
+    logging.info(f"La cantidad de filas del dataframe es: {df.shape[0]}")
+    
     # Porcentaje de null por fila
+    df = df[df.isnull().sum(axis=1)<count_nan]
+    logging.info(f"Eliminacion de NaN >= {count_nan} por fila")
+    logging.info(f"La cantidad de filas del dataframe es: {df.shape[0]}")
 
+    for columna in columns:
+        objetc_column = Columna(settings[columna])
+        
+        # Validacion de unicos
+        if objetc_column.unique:
+            df.drop_duplicates(subset=[columna], inplace=True)
+            logging.info(f"La cantidad de filas del dataframe despues de las validaciones es: {df.shape[0]}")
+        
+        # Validacion de tipo de dato
+        if objetc_column.type == 'int':
+            # Valida enteros, vacíos y nulos
+            df = df[~pd.to_numeric(df[columna], errors='coerce').isnull()]
+        else: # String
+            # Validación de null
+            if objetc_column.not_null:
+                df.dropna(subset=[columna], inplace=True)
+                logging.info(f"La cantidad de filas del dataframe despues de las validaciones es: {df.shape[0]}")
+                
+            # Validacion de regex
+            if objetc_column.regex:
+                df[columna] = df[columna].str.strip() # Elimina espacios
+                df = df[df[columna].str.match(objetc_column.regex)==True]
+    
     # Validar porcentaje de dataframe valido
-
-    # Devolver los datos con errores
+    logging.info(f"La cantidad de filas del dataframe original es: {count_row}")
+    logging.info(f"La cantidad de filas del dataframe despues de las validaciones es: {df.shape[0]}")
+    logging.info(f"Cantidad de filas eliminadas: {count_row - df.shape[0]}")
+    logging.info(f"La calidad del dataframe es del: {(df.shape[0]/count_row)*100}%")
